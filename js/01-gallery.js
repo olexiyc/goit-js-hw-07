176d3927556aa80bfde970ea6073264542f4ebca
Oleksii cherkasov <olexiyc@gmail.com>
2023-03-27 18:11:50 +0800

    Monday

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,50 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-
-// 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-
+const ulElements = document.querySelector('ul');
 const imageContainer = document.querySelector('.gallery');
+const elements = galleryItems.map(({preview, original, description }) => 
+ 
+`<li class="gallery__item">
+<a  class="gallery_link" href='${original}'>
+<img 
+	class="gallery__image" 
+	src= '${preview}' 
+	data-source='${original}' 
+	alt=${description}
+/>
+</a>	
+</li>`
+);
 
-const elements = galleryItems.map((galleryItems) => 
-`<li class="gallery__item"><img class="gallery__image" src= ${galleryItems.preview} alt=${galleryItems.description}></li>`);
+imageContainer.insertAdjacentHTML('afterbegin', elements.join(''));
+imageContainer.addEventListener('click', onPicclick);
 
-console.log(elements)
-imageContainer.insertAdjacentHTML('afterbegin', elements);
+const modalPic = basicLightbox.create(
+	`<img width="1280" height="auto" src="">`, {
+		onShow: (modalPic) => {
+			window.addEventListener('keydown', onEscKeyPress);
+		},
+		onClose: (modalPic) => {
+			window.removeEventListener('keydown', onEscKeyPress);
+		},
+	}
+);
 
+function onPicclick(event) {
+	event.preventDefault();
+	const datasetSource = event.target.dataset.source;
+	if(!datasetSource) return;
+	modalPic.element().querySelector('img').src = datasetSource;
+	modalPic.show();
+};
 
-
-
-// 2. Реалізація делегування на ul.gallery і отримання url великого зображення.
-
-const imageContainerContent = document.querySelector('ul.gallery');
-console.dir(imageContainerContent);
-
-imageContainerContent.addEventListener('click', (event) => {
-    console.log(event.target);
-
-})
-
-
-
-// 3. Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-
-document.querySelector('ul.gallery').onclick = () => {
-
-	basicLightbox.create(`
-		<img width="1400" height="900" src="https://placehold.it/1400x900">
-	`).show()
-
-}
-
-// 4. Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-
-
-
-
-// 5. Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
-
-
-
-
-
+function onEscKeyPress(event){
+	if(event.code !== 'Escape') return;
+	modalPic.close();
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageContainer = document.querySelector('.gallery');
+
+const elements = galleryItems.map(({preview, original, description }) => 
+ 
+`<li class="gallery__item">
+<a  class="gallery_link" href='${original}'>
+<img 
+	class="gallery__image" 
+	src= '${preview}' 
+	data-source='${original}' 
+	alt=${description}
+/>
+</a>	
+</li>`
+);
+
+imageContainer.insertAdjacentHTML('afterbegin', elements.join(''));
+
+const lightbox = new SimpleLightbox ('.gallery a',
+{
+    captionsData: 'alt',
+    cationDelay: 250,
+});
